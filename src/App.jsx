b7d5951875ba608ec7a61b7c25ebf0750e3439cb
Padmanabhan N <padmanabhan@Padmanabhans-MacBook-Pro.local>
2025-07-04 10:05:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     
+     {/* Background Color
+     
+     bg-[color]-[shade]
+     */}
+
+
+<div style={{height:100,width:100, backgroundColor:'red'}}></div>
+
+<div className='divelement'> Text </div>
+
+    </>
+  )
+}
+
+export default App
